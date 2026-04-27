b6080dab821f4d5c4675e49343d6212b440fcb0d
Cédric Belin <cedx@outlook.com>
2026-04-27 11:44:58 +0200

    Add new test cases

--- test/Reflection/ColumnInfo.Tests.ps1
+++ test/Reflection/ColumnInfo.Tests.ps1
@@ -69,4 +69,19 @@ Describe "ColumnInfo" {
 			[ColumnInfo]::new([Character].GetProperty($name)).Type | Should -Be $expected
 		}
 	}
+
+	Context "GetValue" {
+		It "should return the value of the spcified property" {
+			$character = [Character]@{ LastName = "Belin" }
+			[ColumnInfo]::new([Character].GetProperty("LastName")).GetValue($character) | Should -BeExactly "Belin"
+		}
+	}
+
+	Context "SetValue" {
+		It "should set the value of the spcified property" {
+			$character = [Character]@{ LastName = "Belin" }
+			[ColumnInfo]::new([Character].GetProperty("LastName")).SetValue($character, "New NAME")
+			$character.LastName | Should -BeExactly "New NAME"
+		}
+	}
 }
