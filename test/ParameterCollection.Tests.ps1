37b05b2d634c244b40e54c0709cd1b5731ab1f6a
Cédric Belin <cedx@outlook.com>
2026-04-28 23:38:55 +0200

    Add the tests of the `ParameterCollection` class

---
+++ test/ParameterCollection.Tests.ps1
@@ -0,0 +1,61 @@
+using namespace System.Data
+using module ../src/Parameter.psm1
+using module ../src/ParameterCollection.psm1
+
+<#
+.SYNOPSIS
+	Tests the features of the `ParameterCollection` class.
+#>
+Describe "ParameterCollection" {
+	Context "Constructor" {
+		$collection = [ParameterCollection]::new()
+		$collection | Should -BeNullOrEmpty
+
+		$collection = [ParameterCollection]::new([Parameter]@{ Name = "?1"; Value = 123; DbType = [DbType]::Int64 })
+		$collection | Should -HaveCount 1
+
+		$parameter = $collection[0]
+		$parameter.Name | Should -BeExactly "?1"
+		$parameter.Value | Should -Be 123
+		$parameter.DbType | Should -Be ([DbType]::Int64)
+
+		$collection = [ParameterCollection]::new(@(
+			[Parameter]::new("?1", 123)
+			[Parameter]@{ Name = "@Key"; Value = "Unique"; DbType = [DbType]::AnsiString }
+		))
+
+		$collection | Should -HaveCount 2
+
+		# $parameter = $collection[$collection.Count - 1]
+		# $parameter.Name | Should -BeExactly "@Key"
+		# $parameter.Value | Should -BeExactly "Unique"
+		# $parameter.DbType | Should -Be ([DbType]::AnsiString)
+	}
+
+	# Context "Contains" {
+	# 	$collection = new ParameterCollection("@Key")
+	# 	IsTrue($collection.Contains("Key"))
+	# 	IsTrue($collection.Contains("@Key"))
+	# 	IsFalse($collection.Contains("Foo"))
+	# 	IsFalse($collection.Contains("@Foo"))
+	# }
+
+	# Context "IndexOf" {
+	# 	$collection = new ParameterCollection(("?1", 123), ("@Key", "Unique"))
+	# 	AreEqual(1, $collection.IndexOf("Key"))
+	# 	AreEqual(1, $collection.IndexOf("@Key"))
+	# 	AreEqual(-1, $collection.IndexOf("Foo"))
+	# 	AreEqual(-1, $collection.IndexOf("@Foo"))
+	# }
+
+	# Context "RemoveAt" {
+	# 	$collection = new ParameterCollection(("?1", 123), ("@Key", "Unique"))
+	# 	HasCount(2, $collection)
+
+	# 	$collection.RemoveAt("Key")
+	# 	HasCount(1, $collection)
+	# 	Throws<ArgumentOutOfRangeException>(() => $collection.RemoveAt("Foo"))
+	# 	$collection.RemoveAt("?1")
+	# 	IsEmpty($collection)
+	# }
+}
