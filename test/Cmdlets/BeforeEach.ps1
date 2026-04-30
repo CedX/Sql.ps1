a35ad0482e2ee15752788dd9a0770e966e0392b1
Cédric Belin <cedx@outlook.com>
2026-04-30 16:51:41 +0200

    Update the tests

--- test/Cmdlets/BeforeEach.ps1
+++ test/Cmdlets/BeforeEach.ps1
@@ -6,6 +6,6 @@ $connection = [SQLiteConnection] "DataSource=:memory:"
 $connection.Open()
 
 $command = $connection.CreateCommand()
-$command.CommandText = Get-Content "res/Schema.sql" -Raw
+$command.CommandText = Get-Content "$PSScriptRoot/../../res/Schema.sql" -Raw
 $command.ExecuteNonQuery()
 $command.Dispose()
