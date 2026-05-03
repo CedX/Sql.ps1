9acbfd8211317564685675992b8f98cd4b54c2c8
Cédric Belin <cedx@outlook.com>
2026-05-03 17:32:25 +0200

    Add tests for the `Publish-Object` cmdlet

---
+++ test/Cmdlets/Publish-Object.Tests.ps1
@@ -0,0 +1,32 @@
+﻿using module ../Fixtures/Character.psm1
+
+<#
+.SYNOPSIS
+	Tests the features of the `Publish-Object` cmdlet.
+#>
+Describe "Publish-Object" {
+	BeforeAll { Import-Module "$PSScriptRoot/../../Sql.psd1" }
+	BeforeEach { . "$PSScriptRoot/BeforeEach.ps1" }
+	AfterEach { . "$PSScriptRoot/AfterEach.ps1" }
+
+	It "should insert the specified record" {
+		$sql = "SELECT * FROM Characters WHERE firstName = 'Cédric'"
+		Invoke-SqlQuery $connection -As ([Character]) -Command $sql | Should -BeNullOrEmpty
+
+		$character = [Character]@{ FirstName = "Cédric"; LastName = "Belin"; Gender = "Istari" }
+		$character.Id | Should -Be 0
+		$character.FullName | Should -BeNullOrEmpty
+
+		$id = Publish-SqlObject $connection -InputObject $character
+		$id | Should -BeGreaterThan 16
+		$character.Id | Should -Be $id
+
+		$records = Invoke-SqlQuery $connection -As ([Character]) -Command $sql
+		$records | Should -HaveCount 1
+
+		$cedric = $records[0]
+		$cedric.Id | Should -Be $id
+		$cedric.FullName | Should -BeExactly "Cédric Belin"
+		$cedric.Gender | Should -Be $character.Gender
+	}
+}
