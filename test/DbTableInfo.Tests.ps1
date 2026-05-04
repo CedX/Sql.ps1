7e8fb9fa481d79a2f66b6490ae8f7c6d065b9764
Cédric Belin <cedx@outlook.com>
2026-05-04 17:18:41 +0200

    Update the tests

--- test/DbTableInfo.Tests.ps1
+++ test/DbTableInfo.Tests.ps1
@@ -8,7 +8,7 @@ using module ./Fixtures/Character.psm1
 Describe "DbTableInfo" {
 	Context "Columns" {
 		It "should return all columns associated with the specified entity class" {
-			[DbTableInfo]::new([DbTableInfo]).Columns.Keys | Should -BeNullOrEmpty
+			[DbTableInfo]::new([ConsoleKeyInfo]).Columns.Keys | Should -BeNullOrEmpty
 
 			$columns = [DbTableInfo]::new([Character]).Columns
 			$columns.Keys | Should -HaveCount 5
@@ -18,7 +18,7 @@ Describe "DbTableInfo" {
 
 	Context "IdentityColumn" {
 		It "should return the identity column associated with the specified entity class, if any" {
-			[DbTableInfo]::new([DbTableInfo]).IdentityColumn | Should -BeNullOrEmpty
+			[DbTableInfo]::new([ConsoleKeyInfo]).IdentityColumn | Should -BeNullOrEmpty
 
 			$identityColumn = [DbTableInfo]::new([Character]).IdentityColumn
 			$identityColumn | Should -Not -BeNullOrEmpty
@@ -28,7 +28,7 @@ Describe "DbTableInfo" {
 
 	Context "Name" {
 		It "should return the class name when there is no [Table] attribute" {
-			[DbTableInfo]::new([DbTableInfo]).Name | Should -BeExactly DbTableInfo
+			[DbTableInfo]::new([ConsoleKeyInfo]).Name | Should -BeExactly ConsoleKeyInfo
 		}
 
 		It "should return the value of the [Table] attribute when it is present" {
@@ -38,7 +38,7 @@ Describe "DbTableInfo" {
 
 	Context "Schema" {
 		It "should return `$null` when there is no [Table] attribute" {
-			[DbTableInfo]::new([DbTableInfo]).Schema | Should -BeNullOrEmpty
+			[DbTableInfo]::new([ConsoleKeyInfo]).Schema | Should -BeNullOrEmpty
 		}
 
 		It "should return the value of the [Table] attribute when it is present" {
