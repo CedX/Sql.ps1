dbd03bbaafbc26f75d0d1291d14ca7fa913200c4
Cédric Belin <cedx@outlook.com>
2026-06-12 00:47:03 +0200

    Update the build system

--- Scripts/Cmdlets.psm1
+++ Scripts/Cmdlets.psm1
@@ -40,10 +40,11 @@ function Publish-PSGalleryModule {
 	$module = Import-PowerShellDataFile $root/Sql.psd1
 
 	$output = "$root/Temp/PSModule"
-	New-Item $output/Binaries, $output/Sources -ItemType Directory | Out-Null
+	New-Item $output/Binaries, $output -ItemType Directory | Out-Null
 	Copy-Item $root/Sql.psd1 $output/Belin.Sql.psd1
 	Copy-Item $root/*.md $output
-	Copy-Item $root/Sources/*.ps*1 $output/Sources
+	Copy-Item $root/Sources $output -Recurse
+	Remove-Item $output/Sources/*.csproj, $output/Sources/obj -Recurse
 	$module.RequiredAssemblies.ForEach{ "$root/$_" } | Copy-Item -Destination $output/Binaries
 
 	$output = "$root/Temp/PSGallery"
