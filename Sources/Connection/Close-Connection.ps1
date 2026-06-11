c158919e198635e2912543739fb5d25465009983
Cédric Belin <cedx@outlook.com>
2026-06-12 00:49:07 +0200

    Split the connection files

---
+++ Sources/Connection/Close-Connection.ps1
@@ -0,0 +1,26 @@
+using namespace System.Data
+
+<#
+.SYNOPSIS
+	Closes the specified database connection.
+.INPUTS
+	The connection to the data source.
+#>
+function Close-Connection {
+	[CmdletBinding()]
+	[OutputType([void])]
+	param (
+		# The connection to the data source.
+		[Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+		[IDbConnection] $InputObject,
+
+		# Value indicating whether the connection should also be disposed.
+		[switch] $Dispose
+	)
+
+	process {
+		try { $InputObject.Close() }
+		catch { Write-Error $_ }
+		finally { if ($Dispose) { $InputObject.Dispose() } }
+	}
+}
