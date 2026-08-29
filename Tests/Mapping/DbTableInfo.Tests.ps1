83d37274d256a93f6fe54791a55548f07d2190ac
Cédric Belin <cedx@outlook.com>
2026-08-29 15:06:29 +0200

    Code formatting

--- Tests/Mapping/DbTableInfo.Tests.ps1
+++ Tests/Mapping/DbTableInfo.Tests.ps1
@@ -1,4 +1,3 @@
-using namespace Belin.Sql
 using module ../../Sql.psd1
 using module ../Character.psm1
 
@@ -9,9 +8,9 @@ using module ../Character.psm1
 Describe "DbTableInfo" {
 	Context "Columns" {
 		It "should return all columns associated with the specified entity class" {
-			Should-Be 0 ([DbTableInfo]::new([ConsoleKeyInfo]).Columns.Count)
+			Should-Be 0 ([Belin.Sql.DbTableInfo]::new([ConsoleKeyInfo]).Columns.Count)
 
-			$columns = [DbTableInfo]::new([Character]).Columns
+			$columns = [Belin.Sql.DbTableInfo]::new([Character]).Columns
 			Should-Be 5 $columns.Count
 			Should-BeCollection ("firstName", "fullName", "gender", "ID", "lastName") $columns.Keys
 		}
@@ -19,9 +18,9 @@ Describe "DbTableInfo" {
 
 	Context "IdentityColumn" {
 		It "should return the identity column associated with the specified entity class, if any" {
-			Should-BeNull ([DbTableInfo]::new([ConsoleKeyInfo]).IdentityColumn)
+			Should-BeNull ([Belin.Sql.DbTableInfo]::new([ConsoleKeyInfo]).IdentityColumn)
 
-			$identityColumn = [DbTableInfo]::new([Character]).IdentityColumn
+			$identityColumn = [Belin.Sql.DbTableInfo]::new([Character]).IdentityColumn
 			Should-NotBeNull $identityColumn
 			Should-BeString ID $identityColumn.Name -CaseSensitive
 		}
@@ -29,21 +28,21 @@ Describe "DbTableInfo" {
 
 	Context "Name" {
 		It "should return the class name when there is no [Table] attribute" {
-			Should-BeString ConsoleKeyInfo ([DbTableInfo]::new([ConsoleKeyInfo]).Name) -CaseSensitive
+			Should-BeString ConsoleKeyInfo ([Belin.Sql.DbTableInfo]::new([ConsoleKeyInfo]).Name) -CaseSensitive
 		}
 
 		It "should return the value of the [Table] attribute when it is present" {
-			Should-BeString Characters ([DbTableInfo]::new([Character]).Name) -CaseSensitive
+			Should-BeString Characters ([Belin.Sql.DbTableInfo]::new([Character]).Name) -CaseSensitive
 		}
 	}
 
 	Context "Schema" {
 		It "should return `$null` when there is no [Table] attribute" {
-			Should-BeNull ([DbTableInfo]::new([ConsoleKeyInfo]).Schema)
+			Should-BeNull ([Belin.Sql.DbTableInfo]::new([ConsoleKeyInfo]).Schema)
 		}
 
 		It "should return the value of the [Table] attribute when it is present" {
-			Should-BeString main ([DbTableInfo]::new([Character]).Schema) -CaseSensitive
+			Should-BeString main ([Belin.Sql.DbTableInfo]::new([Character]).Schema) -CaseSensitive
 		}
 	}
 }
